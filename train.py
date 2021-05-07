ca6be9b79a78cb3fc4d36e89ba4117d38c74bd67
KLIM_nk01 <56519328+KLIM-nk01@users.noreply.github.com>
2021-05-07 20:50:55 +0300

    Update train.py

--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+  
 """This module implements data feeding and training loop to create model
 to classify X-Ray chest images as a lab example for BSU students.
 """
@@ -13,7 +14,6 @@ import tensorflow as tf
 import time
 from tensorflow.python import keras as keras
 from tensorflow.python.keras.callbacks import LearningRateScheduler
-from tensorflow.keras.applications import EfficientNetB0
 
 # Avoid greedy memory allocation to allow shared GPU usage
 gpus = tf.config.experimental.list_physical_devices('GPU')
@@ -21,11 +21,13 @@ for gpu in gpus:
   tf.config.experimental.set_memory_growth(gpu, True)
 
 
-LOG_DIR = 'logs_rr_0.02'
-BATCH_SIZE = 32
+LOG_DIR = 'logs'
+BATCH_SIZE = 16
 NUM_CLASSES = 101
 RESIZE_TO = 224
 TRAIN_SIZE = 101000
+CROPSIZE_HEIGHT = 290
+CROPSIZE_WIDTH = 270
 
 
 def parse_proto_example(proto):
@@ -36,10 +38,14 @@ def parse_proto_example(proto):
   example = tf.io.parse_single_example(proto, keys_to_features)
   example['image'] = tf.image.decode_jpeg(example['image/encoded'], channels=3)
   example['image'] = tf.image.convert_image_dtype(example['image'], dtype=tf.uint8)
-  example['image'] = tf.image.resize(example['image'], tf.constant([RESIZE_TO, RESIZE_TO]))
+  example['image'] = tf.image.resize(example['image'], tf.constant([CROPSIZE_HEIGHT, CROPSIZE_WIDTH]))
   return example['image'], tf.one_hot(example['image/label'], depth=NUM_CLASSES)
-  
-  
+
+
+def normalize(image, label):
+  return tf.image.per_image_standardization(image), label
+
+
 def create_dataset(filenames, batch_size):
   """Create dataset from tfrecords file
   :tfrecords_files: Mask to collect tfrecords file of dataset
@@ -52,13 +58,15 @@ def create_dataset(filenames, batch_size):
 
 
 def build_model():
-  inputs = tf.keras.Input(shape=(RESIZE_TO, RESIZE_TO, 3))
-  x = tf.keras.layers.experimental.preprocessing.RandomRotation(factor=0.02)(inputs)
-  x = EfficientNetB0(include_top=False, input_tensor = x, pooling ='avg', weights='imagenet')
-  x.trainable = False
-  x = tf.keras.layers.Flatten()(x.output)
-  outputs = tf.keras.layers.Dense(NUM_CLASSES, activation=tf.keras.activations.softmax)(x)
-  return tf.keras.Model(inputs=inputs, outputs=outputs)
+    inputs = tf.keras.Input(shape=(CROPSIZE_HEIGHT, CROPSIZE_WIDTH, 3))
+    data_augmentation = tf.keras.layers.experimental.preprocessing.RandomCrop(RESIZE_TO, RESIZE_TO)(inputs)
+    data_augmentation = tf.keras.layers.experimental.preprocessing.RandomFlip(mode="horizontal",seed=None,name=None)(data_augmentation)
+    data_augmentation = tf.keras.layers.experimental.preprocessing.RandomRotation(factor=0.04, fill_mode='reflect')(data_augmentation)
+    model = tf.keras.applications.EfficientNetB0(include_top=False, input_tensor=data_augmentation, weights='imagenet')
+    model.trainable = False
+    x = tf.keras.layers.GlobalAveragePooling2D()(model.output)
+    outputs = tf.keras.layers.Dense(NUM_CLASSES, activation=tf.keras.activations.softmax)(x)
+    return tf.keras.Model(inputs=inputs, outputs=outputs)
 
 
 def main():
@@ -72,8 +80,7 @@ def main():
   validation_dataset = dataset.skip(train_size)
 
   model = build_model()
-
-
+ 
   model.compile(
     optimizer=tf.optimizers.Adam(0.0001),
     loss=tf.keras.losses.categorical_crossentropy,
@@ -83,10 +90,10 @@ def main():
   log_dir='{}/f101-{}'.format(LOG_DIR, time.time())
   model.fit(
     train_dataset,
-    epochs=20,
+    epochs=50,
     validation_data=validation_dataset,
     callbacks=[
-      tf.keras.callbacks.TensorBoard(log_dir)
+      tf.keras.callbacks.TensorBoard(log_dir),
     ]
   )
 
