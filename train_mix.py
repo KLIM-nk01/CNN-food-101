2d51ba84c9b2736eba2f36b4a312e4fb2729c94e
KLIM_nk01 <56519328+KLIM-nk01@users.noreply.github.com>
2021-05-16 00:04:05 +0300

    Update train_mix.py

--- train_mix.py
+++ train_mix.py
@@ -1,4 +1,3 @@
-  
 """This module implements data feeding and training loop to create model
 to classify X-Ray chest images as a lab example for BSU students.
 """
@@ -14,6 +13,8 @@ import tensorflow as tf
 import time
 from tensorflow.python import keras as keras
 from tensorflow.python.keras.callbacks import LearningRateScheduler
+from tensorflow.keras.applications import EfficientNetB0
+from PIL import Image
 
 # Avoid greedy memory allocation to allow shared GPU usage
 gpus = tf.config.experimental.list_physical_devices('GPU')
@@ -21,13 +22,11 @@ for gpu in gpus:
   tf.config.experimental.set_memory_growth(gpu, True)
 
 
-LOG_DIR = 'logs_mix'
-BATCH_SIZE = 16
+LOG_DIR = 'logs'
+BATCH_SIZE = 32
 NUM_CLASSES = 101
 RESIZE_TO = 224
 TRAIN_SIZE = 101000
-CR_HEIGHT = 290
-CR_WIDTH = 270
 
 
 def parse_proto_example(proto):
@@ -38,14 +37,14 @@ def parse_proto_example(proto):
   example = tf.io.parse_single_example(proto, keys_to_features)
   example['image'] = tf.image.decode_jpeg(example['image/encoded'], channels=3)
   example['image'] = tf.image.convert_image_dtype(example['image'], dtype=tf.uint8)
-  example['image'] = tf.image.resize(example['image'], tf.constant([CR_HEIGHT, CR_WIDTH]))
+  example['image'] = tf.image.resize(example['image'], tf.constant([290, 270]))
   return example['image'], tf.one_hot(example['image/label'], depth=NUM_CLASSES)
 
 
-def normalize(image, label):
-  return tf.image.per_image_standardization(image), label
-
-
+def process_data(image, label):
+  return tf.image.random_crop(image, [224, 224, 3]), label
+  
+  
 def create_dataset(filenames, batch_size):
   """Create dataset from tfrecords file
   :tfrecords_files: Mask to collect tfrecords file of dataset
@@ -53,21 +52,27 @@ def create_dataset(filenames, batch_size):
   """
   return tf.data.TFRecordDataset(filenames)\
     .map(parse_proto_example, num_parallel_calls=tf.data.AUTOTUNE)\
+    .map(process_data)\
     .batch(batch_size)\
     .prefetch(tf.data.AUTOTUNE)
 
 
 def build_model():
-    inputs = tf.keras.Input(shape=(CR_HEIGHT, CR_WIDTH, 3))
-    data_augmentation = tf.keras.layers.experimental.preprocessing.RandomCrop(RESIZE_TO, RESIZE_TO)(inputs)
-    data_augmentation = tf.keras.layers.experimental.preprocessing.RandomFlip(mode="horizontal",seed=None,name=None)(data_augmentation)
-    data_augmentation = tf.keras.layers.experimental.preprocessing.RandomRotation(factor=0.04, fill_mode='reflect')(data_augmentation)
-    model = tf.keras.applications.EfficientNetB0(include_top=False, input_tensor=data_augmentation, weights='imagenet')
-    model.trainable = False
-    x = tf.keras.layers.GlobalAveragePooling2D()(model.output)
-    outputs = tf.keras.layers.Dense(NUM_CLASSES, activation=tf.keras.activations.softmax)(x)
-    return tf.keras.Model(inputs=inputs, outputs=outputs)
+  inputs = tf.keras.Input(shape=(224, 224, 3))
+  img_aug = tf.keras.layers.experimental.preprocessing.RandomFlip("horizontal")(inputs)
+  img_aug = tf.keras.layers.experimental.preprocessing.RandomRotation(factor=0.04, fill_mode='reflect')(img_aug)
+  x = EfficientNetB0(include_top=False, input_tensor = img_aug,pooling ='avg', weights='imagenet')
+  x.trainable = False
+  x = tf.keras.layers.Flatten()(x.output)
+  outputs = tf.keras.layers.Dense(NUM_CLASSES, activation=tf.keras.activations.softmax)(x)
+  return tf.keras.Model(inputs=inputs, outputs=outputs)
 
+
+def unfreeze_model(model):
+  for layer in model.layers:
+    if not isinstance(layer, tf.keras.layers.BatchNormalization):
+      layer.trainable = True
+      
 
 def main():
   args = argparse.ArgumentParser()
@@ -80,7 +85,7 @@ def main():
   validation_dataset = dataset.skip(train_size)
 
   model = build_model()
- 
+
   model.compile(
     optimizer=tf.optimizers.Adam(0.0001),
     loss=tf.keras.losses.categorical_crossentropy,
@@ -93,10 +98,25 @@ def main():
     epochs=50,
     validation_data=validation_dataset,
     callbacks=[
+      tf.keras.callbacks.TensorBoard(log_dir)
+    ]
+  )
+  
+  unfreeze_model(model)
+
+  model.compile(
+    optimizer=tf.optimizers.Adam(lr=1e-7),
+    loss=tf.keras.losses.categorical_crossentropy,
+    metrics=[tf.keras.metrics.categorical_accuracy],
+  )
+  model.fit(
+    train_dataset,
+    epochs=20,
+    validation_data=validation_dataset,
+    callbacks=[
       tf.keras.callbacks.TensorBoard(log_dir),
     ]
   )
-
 
 if __name__ == '__main__':
     main()
